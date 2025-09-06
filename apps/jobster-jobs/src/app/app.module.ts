39edb982856ffb7b1fe9f72c07ea7c27e9c21875
adibrisan <adi.brisan@yahoo.com>
2025-09-06 17:57:07 +0300

    create & get jobs

--- apps/jobster-jobs/src/app/app.module.ts
+++ apps/jobster-jobs/src/app/app.module.ts
@@ -1,9 +1,23 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JobsModule } from './jobs/jobs.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ConfigModule, JobsModule],
+  imports: [
+    ConfigModule,
+    JobsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
