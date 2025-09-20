9e26be0b7ceaa7f6c86b3455c25bb810f2919003
adibrisan <adi.brisan@yahoo.com>
2025-09-20 15:51:43 +0300

    send a custom object input from pulsar

--- apps/jobster-executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
+++ apps/jobster-executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
@@ -1,9 +1,9 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PulsarClient, PulsarConsumer } from '@jobster/pulsar';
 import { Message } from 'pulsar-client';
 
 @Injectable()
-export class FibonacciConsumer extends PulsarConsumer implements OnModuleInit {
+export class FibonacciConsumer extends PulsarConsumer {
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient, 'Fibonacci');
   }
