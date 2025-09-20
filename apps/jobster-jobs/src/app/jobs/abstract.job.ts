81c3ea9acb49e0d65a9f6038679cc6879f2cb87d
adibrisan <adi.brisan@yahoo.com>
2025-09-20 11:56:06 +0300

    create the subscription type and add logic of consumer

--- apps/jobster-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobster-jobs/src/app/jobs/abstract.job.ts
@@ -1,8 +1,7 @@
 import { PulsarClient } from '@jobster/pulsar';
-import { OnModuleDestroy } from '@nestjs/common';
 import { Producer } from 'pulsar-client';
 
-export abstract class AbstractJob<T> implements OnModuleDestroy {
+export abstract class AbstractJob<T> {
   private producer: Producer;
 
   constructor(private readonly pulsarClient: PulsarClient) {}
@@ -14,9 +13,5 @@ export abstract class AbstractJob<T> implements OnModuleDestroy {
     await this.producer.send({
       data: Buffer.from(JSON.stringify(data)),
     });
-  }
-
-  async onModuleDestroy() {
-    await this.producer.close();
   }
 }
--- libs/pulsar/src/lib/pulsar.client.ts
+++ libs/pulsar/src/lib/pulsar.client.ts
@@ -1,22 +1,39 @@
 import { ConfigService } from '@nestjs/config';
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
-import { Client } from 'pulsar-client';
+import { Client, Consumer, Message, Producer } from 'pulsar-client';
 
 @Injectable()
 export class PulsarClient implements OnModuleDestroy {
   private readonly client = new Client({
     serviceUrl: this.configService.getOrThrow('PULSAR_SERVICE_URL'),
   });
+  private readonly producers: Producer[] = [];
+  private readonly consumers: Consumer[] = [];
 
   constructor(private readonly configService: ConfigService) {}
 
   async createProducer(topic: string) {
-    return this.client.createProducer({
+    const producer = await this.client.createProducer({
       topic,
     });
+    this.producers.push(producer);
+    return producer;
+  }
+
+  async createConsumer(topic: string, listener: (message: Message) => void) {
+    const consumer = await this.client.subscribe({
+      topic,
+      subscription: 'jobster',
+      listener,
+    });
+    this.consumers.push(consumer);
+    return consumer;
   }
 
   async onModuleDestroy() {
+    for (const producer of this.producers) {
+      await producer.close();
+    }
     await this.client.close();
   }
 }
