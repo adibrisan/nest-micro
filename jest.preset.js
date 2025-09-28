007e6e5fbd8c1de27245d0bc3db44d269c1abefd
adibrisan <adi.brisan@yahoo.com>
2025-09-28 15:42:31 +0300

    config jest preset for uuid

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,7 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset, passWithNoTests: true };
+module.exports = {
+  ...nxPreset,
+  transformIgnorePatterns: ['node_modules/(?!uuid)'],
+  passWithNoTests: true,
+};
