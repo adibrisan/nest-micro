de78d8d3951c0c43e469d90b21c76b5fceb21aa2
adibrisan <adi.brisan@yahoo.com>
2025-10-06 20:40:05 +0300

    fix jwt expiration in k8s & add url for grpc client

--- apps/jobs/src/app/jobs.module.ts
+++ apps/jobs/src/app/jobs.module.ts
@@ -8,22 +8,27 @@ import { FibonacciJob } from './jobs/fibonacci/fibonacci.job';
 import { JobsService } from './jobs.service';
 import { JobsResolver } from './jobs.resolver';
 import { PulsarModule } from '@jobster/pulsar';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     DiscoveryModule,
     PulsarModule,
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: AUTH_PACKAGE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          package: AUTH_PACKAGE_NAME,
-          protoPath: join(
-            __dirname,
-            '../../../libs/grpc/src/lib/proto/auth.proto'
-          ),
-        },
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: configService.getOrThrow('AUTH_GRPC_SERVICE_URL'),
+            package: AUTH_PACKAGE_NAME,
+            protoPath: join(
+              __dirname,
+              '../../../libs/grpc/src/lib/proto/auth.proto'
+            ),
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
