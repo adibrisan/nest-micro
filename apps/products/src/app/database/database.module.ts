ce532046472791f369cf0d1ebe1d1e8cac08ed80
adibrisan <adi.brisan@yahoo.com>
2025-10-10 22:05:31 +0300

    config products service

---
+++ apps/products/src/app/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Global, Module } from '@nestjs/common';
+import { DATABASE_CONNECTION } from './database-connection';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { ConfigService } from '@nestjs/config';
+import { products } from '../products/schema';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: DATABASE_CONNECTION,
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+        });
+        return drizzle(pool, {
+          schema: {
+            ...products,
+          },
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: [DATABASE_CONNECTION],
+})
+export class DatabaseModule {}
