f6cf7aed1c7f3a2f1f82b09374fd895bdd3c9297
adibrisan <adi.brisan@yahoo.com>
2025-10-13 20:03:39 +0300

    execute jobs from file upload and store to db

--- apps/executor/src/app/jobs/jobs.module.ts
+++ apps/executor/src/app/jobs/jobs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FibonacciConsumer } from './fibonacci/fibonacci.consumer';
 import { PulsarModule } from '@jobster/pulsar';
+import { LoadProductsModule } from './products/load-products.module';
 
 @Module({
-  imports: [PulsarModule],
+  imports: [PulsarModule, LoadProductsModule],
   providers: [FibonacciConsumer],
 })
 export class JobsModule {}
---
+++ apps/executor/src/app/jobs/products/load-products.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { LoadProductsConsumer } from './load-products.consumer';
+import { PulsarModule } from '@jobster/pulsar';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { Packages } from '@jobster/grpc';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    PulsarModule,
+    ClientsModule.registerAsync([
+      {
+        name: Packages.PRODUCTS,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: configService.getOrThrow('PRODUCTS_GRPC_SERVICE_URL'),
+            package: Packages.PRODUCTS,
+            protoPath: join(
+              __dirname,
+              '../../../libs/grpc/src/lib/proto/products.proto'
+            ),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
+  providers: [LoadProductsConsumer],
+})
+export class LoadProductsModule {}
